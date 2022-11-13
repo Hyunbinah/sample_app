5c4166204edc075eb6cc4c404592446e1f7f35da
hyunbinah <hyunbin.kim1105@gmail.com>
2022-11-13 14:33:23 +0800

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  host = 'example.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
